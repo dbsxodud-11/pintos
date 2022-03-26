9b2f2522b8c6ced8346b57b7c2c82789de3193a5
dbsxodud-11 <99yty@kaist.ac.kr>
2022-03-26 06:34:30 +0000

    prevent print page fault message

--- userprog/exception.c
+++ userprog/exception.c
@@ -149,6 +149,10 @@ page_fault (struct intr_frame *f) {
 	/* Count page faults. */
 	page_fault_cnt++;
 
+#ifdef USERPROG
+	exit(-1);
+#endif
+
 	/* If the fault is true fault, show info and exit. */
 	printf ("Page fault at %p: %s error %s page in %s context.\n",
 			fault_addr,
