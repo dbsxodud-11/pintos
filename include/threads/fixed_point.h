73760e988101cdfd8eb26bef7ae3c7857e69f92b
dbsxodud-11 <99yty@kaist.ac.kr>
2022-04-28 06:35:32 +0000

    fix error in proj1

--- include/threads/fixed_point.h
+++ include/threads/fixed_point.h
@@ -1,8 +1,8 @@
 /* For Fixed-Point Arithmetic */
 #include <stdint.h>
 
-#define q 14
-#define f (1<<14)
+#define Q 14
+#define F (1<<14)
 
 int convert_n_to_fp (int n);
 int convert_x_to_int (int x);
@@ -23,16 +23,16 @@ int div_x_by_n (int x, int n);
 /* 이런 방식으로 쭉 구현하셈 */
 
 int convert_n_to_fp (int n) {
-    return n * f;
+    return n * F;
 }
 
 int convert_x_to_int (int x) {
-    return x / f;
+    return x / F;
 }
 
 int convert_x_to_int_round (int x) {
-    if (x >= 0) return (x + (f / 2)) / f;
-    return (x - (f / 2)) / f;
+    if (x >= 0) return (x + (F / 2)) / F;
+    return (x - (F / 2)) / F;
 }
 
 int add_x_and_y (int x, int y) {
@@ -44,15 +44,15 @@ int sub_y_from_x (int x, int y) {
 }
 
 int add_x_and_n (int x, int n) {
-    return x + (n * f);
+    return x + (n * F);
 }
 
 int sub_n_from_x (int x, int n) {
-    return x - (n * f);
+    return x - (n * F);
 }
 
 int mul_x_by_y (int x, int y) {
-    return ((int64_t) x) * y / f;
+    return ((int64_t) x) * y / F;
 }
 
 int mul_x_by_n (int x, int n) {
@@ -60,7 +60,7 @@ int mul_x_by_n (int x, int n) {
 }
 
 int div_x_by_y (int x, int y) {
-    return ((int64_t) x) * f / y;
+    return ((int64_t) x) * F / y;
 }
 
 int div_x_by_n (int x, int n) {
